e3c84714a5770c1e4bb2a90d68796ac3c7d26462
Tiago Moura <tiagodefmoura@gmail.com>
2025-02-09 01:43:31 -0300

    atualizações no MakeList

--- DisplayC.c
+++ DisplayC.c
@@ -15,13 +15,16 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/stdio.h"
+#include "hardware/pio.h"
+#include "DisplayC.pio.h"
+
 #include "hardware/i2c.h"
 #include "inc/ssd1306.h"
 #include "inc/font.h"
 
-//#include "hardware/pio.h"
+
 #include "hardware/clocks.h"
-//#include "ws2812.pio.h"
+#include "DisplayC.pio.h"
 
 #include "hardware/uart.h"
 
@@ -43,7 +46,15 @@ static void gpio_irq_handler(uint gpio, uint32_t events){
 //dentro da main
 
     
+//configuração necessária para matriz de leds
+#define IS_RGBW false
+#define NUM_PIXELS 25
+#define WS2812_PIN 7
 
+// Variável global para armazenar a cor (Entre 0 e 255 para intensidade)
+uint8_t led_r = 0; // Intensidade do vermelho
+uint8_t led_g = 0; // Intensidade do verde
+uint8_t led_b = 20; // Intensidade do azul
 
 #define I2C_PORT i2c1
 #define I2C_SDA 14
@@ -54,6 +65,107 @@ static void gpio_irq_handler(uint gpio, uint32_t events){
 #define led_pin_b 12
 #define led_pin_r 13
 //
+static volatile uint a = 1;
+static volatile uint32_t last_time = 0; // Armazena o tempo do último evento (em microssegundos)
+
+// Prototipação da função de interrupção
+static void gpio_irq_handler(uint gpio, uint32_t events);
+
+// Representação de números de 0 a 9 para a matriz 5x5
+bool numeros[10][NUM_PIXELS] = {
+    
+  {1, 1, 1, 1, 1,
+   1, 0, 0, 0, 1,
+   1, 0, 0, 0, 1,
+   1, 0, 0, 0, 1, 
+   1, 1, 1, 1, 1}, // 0
+
+  {0, 1, 0, 0, 0,
+   0, 0, 0, 1, 0,
+   0, 1, 0, 0, 0, 
+   0, 0, 0, 1, 0,
+   0, 1, 0, 0, 0}, // 1
+
+  {1, 1, 1, 1, 1, 
+   1, 0, 0, 0, 0,
+   1, 1, 1, 1, 1,
+   0, 0, 0, 0, 1,
+   1, 1, 1, 1, 1}, // 2
+
+  {1, 1, 1, 1, 1, 
+   0, 0, 0, 0, 1,
+   1, 1, 1, 1, 1,
+   0, 0, 0, 0, 1,
+   1, 1, 1, 1, 1}, // 3
+
+  {0, 1, 0, 0, 0,
+   0, 0, 0, 1, 0,
+   0, 1, 1, 1, 1, 
+   1, 0, 0, 1, 0,
+   0, 1, 0, 0, 1}, // 4
+
+  {1, 1, 1, 1, 1, 
+   0, 0, 0, 0, 1,
+   1, 1, 1, 1, 1,
+   1, 0, 0, 0, 0,
+   1, 1, 1, 1, 1}, // 5
+
+  {1, 1, 1, 1, 1, 
+   1, 0, 0, 0, 1,
+   1, 1, 1, 1, 1,
+   1, 0, 0, 0, 0,
+   1, 1, 1, 1, 1}, // 6
+
+  {0, 1, 0, 0, 0,
+   0, 0, 0, 1, 0,
+   0, 1, 0, 0, 0, 
+   0, 0, 0, 1, 0,
+   0, 1, 1, 1, 1}, // 7
+
+  {1, 1, 1, 1, 1, 
+   1, 0, 0, 0, 1,
+   1, 1, 1, 1, 1,
+   1, 0, 0, 0, 1,
+   1, 1, 1, 1, 1}, // 8
+   
+  {1, 1, 1, 1, 1, 
+   0, 0, 0, 0, 1,
+   1, 1, 1, 1, 1,
+   1, 0, 0, 0, 1,
+   1, 1, 1, 1, 1}  // 9
+};
+
+// Variável global para armazenar o número atual exibido
+//int numero_atual = NUMERO_INICIAL;
+
+static inline void put_pixel(uint32_t pixel_grb)
+{
+    pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
+}
+
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b)
+{
+    return ((uint32_t)(r) << 8) | ((uint32_t)(g) << 16) | (uint32_t)(b);
+}
+
+
+void set_number_on_leds(int num)
+{
+    for (int i = 0; i < NUM_PIXELS; i++)
+    {
+        if (numeros[num][i])
+        {
+            put_pixel(urgb_u32(led_r, led_g, led_b));  // Acende o LED com a cor atual
+        }
+        else
+        {
+            put_pixel(0);  // Apaga o LED
+        }
+    }
+}
+
+
+
 int main()
 {//parte do codigo para bootsel com botao
 //inicializar o botão de interrupção - GPIO5
@@ -99,6 +211,12 @@ stdio_init_all(); // Inicializa comunicação USB CDC para monitor serial
   ssd1306_send_data(&ssd);
 
   bool cor = true;
+  //Parte ws2812
+  PIO pio = pio0;
+  int sm = 0;
+  uint offset = pio_add_program(pio, &ws2812_program);
+  ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
+
   while (true)
   {
     cor = !cor;
@@ -112,9 +230,9 @@ if (stdio_usb_connected())
     if (scanf("%c", &c) == 1)
     { // Lê caractere da entrada padrão
         printf("Recebido: '%c'\n", c);
-        if ((c >= 'A' && c <= 'Z')||(c >= 'a' && c <= 'z')||(c >= '0' && c <= '9')){
+        if ((c >= 'A' && c <= 'Z')||(c >= 'a' && c <= 'z')){
 
-
+           
         
         //switch (c)
         //{
@@ -130,17 +248,28 @@ if (stdio_usb_connected())
        // case 'g':
        // ssd1306_draw_char(&ssd, c, 8, 10);
         //ssd1306_draw_string(&ssd, "g", 8, 10); // Desenha uma string
+      
+        //alternar led verde
          //   gpio_put(led_pin_g, !gpio_get(led_pin_g));
          //   printf("LED verde alternado!\n");
           //  break;
        // case 'b':
-       // ssd1306_draw_char(&ssd, c, 8, 10);
-       // ssd1306_draw_string(&ssd, "b", 8, 10); // Desenha uma string
-         //   gpio_put(led_pin_b, !gpio_get(led_pin_b));
+       //   gpio_put(led_pin_b, !gpio_get(led_pin_b));
           //  printf("LED azul alternado!\n");
           //  break;
+
+       // ssd1306_draw_char(&ssd, c, 8, 10);
+       // ssd1306_draw_string(&ssd, "b", 8, 10); // Desenha uma string
+         
       //  default:
             //printf("Comando inválido: '%c'\n", c);
+        }else if (c >= '0' && c <= '9'){
+          int num = c - '0';
+          ssd1306_draw_char(&ssd, c, 8, 10);
+          set_number_on_leds(num);
+
+
+
         }else{
           printf("Comando inválido: '%c'\n", c);
           ssd1306_draw_string(&ssd, "Comando invalido", 8, 10); // Desenha uma string
